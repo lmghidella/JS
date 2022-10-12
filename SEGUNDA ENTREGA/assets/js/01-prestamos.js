45baa3a79db34b8525f3189d974903f3a0c71463
lmghidella <96829337+lmghidella@users.noreply.github.com>
2022-10-12 00:48:22 -0300

    idem primer entrega con clase y objeto

--- SEGUNDA ENTREGA/assets/js/01-prestamos.js
+++ SEGUNDA ENTREGA/assets/js/01-prestamos.js
@@ -1,3 +1,33 @@
+class Prestamo { 
+    constructor(capital, interes,cuotas,amortizacion) {
+        this.capital = capital;
+        this.interes = interes;
+        this.cuotas=cuotas; 
+        this.amortizacion;
+    }
+    calcular()
+    {
+    let total_prestamo=0;
+    let saldo=capital;
+    let cuota_pura=capital/cuotas;
+    let valor_cuota=cuota_pura+cuota_pura*tasa/100;
+    document.write('<h5> Sistema de amortización: '+amortizacion+'</h5>');//POR AHORA NO DISCRIMINO CALULO ENTRE
+    //SISTEMA FRANCES / ALEMAN
+    for (let i=0;i<cuotas;i++){
+        console.log('Cuota nº', i+1);
+        console.log('Valor Cuota: ',valor_cuota.toFixed(2));
+        saldo=saldo-cuota_pura;
+        console.log('Saldo Capital: ', saldo.toFixed(2));
+        document.write('<p>Valor de cuota nº ' +(i+1)+': $'+valor_cuota.toFixed(2) +'-----------------Saldo de capital: $'+saldo.toFixed(2)+'</p>');
+        total_prestamo=total_prestamo+valor_cuota;
+    }
+    console.log('Total prestamo',total_prestamo);
+    document.write('<h5> Capital Solicitado: $'+capital+'</h5>');
+    return total_prestamo;
+    }
+}
+
+
 let capital = prompt('Ingrese el capital a solicitar en $: ');
 let tasa= prompt('Ingrese la tasa de Interes por cuota en %');
 let amortizacion;
@@ -13,30 +43,14 @@ do{
     
 }while((amortizacion!='FRANCES' && amortizacion!='ALEMAN'))
 
+
 do {
 cuotas=prompt('Ingrese la cantidad de cuotas');
 }while(cuotas==0);//verifico que la cantidad de cuotas no sea igual a 0
 //Aclaro que el metodo de calculo no aplica exactamente una amortizacion frances/ aleman sino que uso un sistema de
 //interes X para probar lo aprendido hasta el momento.
-document.write('<h4> Valor total del prestamo: $'+calcular(capital,tasa,amortizacion,cuotas).toFixed(2)+'   -----<h4>');
+let prestamo1=new Prestamo (capital, tasa,cuotas,amortizacion);
+document.write('<h4> Valor total del prestamo: $'+prestamo1.calcular().toFixed(2)+'   -----<h4>');
 
-function calcular(capital,tasa,amortizacion,cuotas)
-{
-let total_prestamo=0;
-let saldo=capital;
-let cuota_pura=capital/cuotas;
-let valor_cuota=cuota_pura+cuota_pura*tasa/100;
-document.write('<h5> Sistema de amortización: '+amortizacion+'</h5>');//POR AHORA NO DISCRIMINO CALULO ENTRE
-//SISTEMA FRANCES / ALEMAN
-for (let i=0;i<cuotas;i++){
-    console.log('Cuota nº', i+1);
-    console.log('Valor Cuota: ',valor_cuota.toFixed(2));
-    saldo=saldo-cuota_pura;
-    console.log('Saldo Capital: ', saldo.toFixed(2));
-    document.write('<p>Valor de cuota nº ' +(i+1)+': $'+valor_cuota.toFixed(2) +'-----------------Saldo de capital: $'+saldo.toFixed(2)+'</p>');
-    total_prestamo=total_prestamo+valor_cuota;
-}
-console.log('Total prestamo',total_prestamo);
-document.write('<h5> Capital Solicitado: $'+capital+'</h5>');
-return total_prestamo;
-}
+
+
